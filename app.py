9960a0035292ba700e7593f4dfb9814cfbddddde
Rodrigo Guedes <rodrigo.guedes@semantix.com.br>
2019-10-23 11:33:47 -0300

    Novos ajustes

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 app = Flask(__name__)
+
+chatbot = ChatBot("Charlie", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.portuguese")
+
+'''
+chatbot = ChatBot(
+    'Charlie', 
+    #logic_adapters=['chatterbot.logic.BestMatch'], 
+    database_uri="sqlite:///db.sqlite3"
+)
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+    "Olá",
+    "Olá!",
+    "Como vai você?",
+    "Estou ótimo",
+    "Isso é bom de ouvir",
+    "Obrigado.",
+    "De nada."
+])
+'''
 
 @app.route('/')
 def index():
@@ -10,23 +34,9 @@ def index():
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
-    chatbot = ChatBot(
-        'Charlie', 
-        logic_adapters=['chatterbot.logic.BestMatch'], 
-        database_uri="sqlite:///db.sqlite3"
-    )
-
-    trainer = ListTrainer(chatbot)
-
-    trainer.train([
-        "Olá",
-        "Olá!",
-        "Como vai você?",
-        "Estou ótimo",
-        "Isso é bom de ouvir",
-        "Obrigado.",
-        "De nada."
-    ])
-
-    response = chatbot.get_response(str(request.form.get('text')))
+    msg = str(request.form.get('text'))
+    response = chatbot.get_response(msg)
     return str(response)
+
+if __name__ == "__main__":
+    app.run()
